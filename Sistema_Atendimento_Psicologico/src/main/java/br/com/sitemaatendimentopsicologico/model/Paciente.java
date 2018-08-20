7750c2e10ea6a9c9fff8624f6373f36aeca11b33
caiovpalmeidadjr <Kv311224>
2018-08-20 16:03:23 -0300

    Implementação de classes do model

---
+++ Sistema_Atendimento_Psicologico/src/main/java/br/com/sitemaatendimentopsicologico/model/Paciente.java
@@ -0,0 +1,89 @@
+package br.com.sitemaatendimentopsicologico.model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+	private Long id;
+	private String nome;
+	private LocalDate dataDeNascimento;
+	private String telefone;
+	private String celular;
+	private String email;
+	private String queixa;
+	private Consulta consulta;
+	private Doenca doenca;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getQueixa() {
+		return queixa;
+	}
+
+	public void setQueixa(String queixa) {
+		this.queixa = queixa;
+	}
+
+	public Consulta getConsulta() {
+		return consulta;
+	}
+
+	public void setConsulta(Consulta consulta) {
+		this.consulta = consulta;
+	}
+
+	public Doenca getDoenca() {
+		return doenca;
+	}
+
+	public void setDoenca(Doenca doenca) {
+		this.doenca = doenca;
+	}
+
+}
